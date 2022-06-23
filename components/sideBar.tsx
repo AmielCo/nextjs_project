ac3268bfe76201d83ead528cab496a73d305518f
AmielCo <amiel.coronel@gmail.com>
2022-06-23 19:04:17 +0300

    setting up the navBar

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -16,6 +16,25 @@ import {
   MdFavorite,
 } from "react-icons/md";
 import NextImage from "next/image";
+import NextLink from "next/link";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
 
 const Sidebar = () => {
   return (
@@ -29,6 +48,26 @@ const Sidebar = () => {
       <Box paddingY="20px">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/Logo.svg" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
