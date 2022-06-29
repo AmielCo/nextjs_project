e63acdb16d3a308be01aa99034faf89590d13ab9
AmielCo <amiel.coronel@gmail.com>
2022-06-29 18:26:47 +0300

    creating custom hooks and finalizing protected routes

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
 
-const signedinPages = ['/', '/playlist', '/library'];
+const signedinPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req) {
-    if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-        const token = req.cookies[process.env.TOKEN_NAME];
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies[process.env.TOKEN_NAME];
 
-        if (!token) {
-            return NextResponse.redirect('/signin');
-        }
+    if (!token) {
+      return NextResponse.redirect("/signin");
     }
-        
+  }
 }
